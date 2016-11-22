c53ebdc0eae63fdd69f1bad09a57f8affdf93fd1
Jannick Berkhout <jannickberkhout@hotmail.com>
2016-11-22 22:53:35 +0100

    Added vue resource

--- js/app.js
+++ js/app.js
@@ -21,12 +21,28 @@ Vue.component('vp-content', {
 });
 
 Vue.component('vp-section', {
+  data: function() {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    fetchContent() {
+      this.$http.get('https://jsonplaceholder.typicode.com/posts')
+        .then(function(data) {
+          var random   = Math.round(Math.random() * (9 - 0) + 0);
+          this.content = data.body[random].body;
+        });
+    }
+  },
   props: ['type'],
   template: 
     `<section class="section" :class="{ 'section-dark': type }">
       <vp-content>
         <slot name="title"></slot>
-        <slot></slot>
+        {{ content }}
+
+        <button @click="fetchContent()">Fetch new content</button>
       </vp-content>
     </section>`
 })
@@ -35,6 +51,7 @@ var app = new Vue({
   el: '#app',
 
   data: {
+    content: '',
     items: [
       { text: 'Item 1' },
       { text: 'Item 2' },
